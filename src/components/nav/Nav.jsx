5c3cbc18abb304b49d31bf2be0a5fb558226d473
SyedMusaGillani <musa.gillani@devsinc.com>
2022-09-26 02:28:25 +0500

    reduced nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,32 +3,35 @@ import "./nav.css";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-sm bg-color navbar-dark">
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+    <nav className="navbar navbar-expand-sm bg-color navbar-dark container-fluid">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon white"></span>
+      </button>
+      <a className="navbar-brand" href="#goes-to-nothing">
+        <img src={logo} alt="Burger-logo" width="30" height="24" />
+      </a>
+      <div
+        className="collapse navbar-collapse navbar-nav "
+        id="navbarSupportedContent"
+      >
+        <a
+          className="nav-link white ms-auto"
+          aria-current="page"
+          href="#Burger"
         >
-          <span className="navbar-toggler-icon white"></span>
-        </button>
-        <a className="navbar-brand" href="#goes-to-nothing">
-          <img src={logo} alt="Burger-logo" width="30" height="24" />
+          Burger Builder
         </a>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <div className="navbar-nav ms-auto">
-            <a className="nav-link white" aria-current="page" href="#Burger">
-              Burger Builder
-            </a>
-            <a className="nav-link white" href="#Login">
-              Login
-            </a>
-          </div>
-        </div>
+        <a className="nav-link white" href="#Login">
+          Login
+        </a>
       </div>
     </nav>
   );
