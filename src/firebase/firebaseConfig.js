ca35dd07f8d14c7db2640aa087cfd8524d4dbed7
SyedMusaGillani <musa.gillani@devsinc.com>
2022-10-07 16:28:51 +0500

    added firebase package configured firebase app added hosting from github actions

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDipQFLCfo8oJKQl5ypOCjPACr5ii-33KQ",
+  authDomain: "burger-app-1f7d8.firebaseapp.com",
+  projectId: "burger-app-1f7d8",
+  storageBucket: "burger-app-1f7d8.appspot.com",
+  messagingSenderId: "463980882098",
+  appId: "1:463980882098:web:17d11f01ad8f34c72081ab",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
